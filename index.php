6ab18ab158dcfa42e483e94d592e8a926b21940c
Devin Liang <devinliang@gmail.com>
2021-03-07 23:22:20 +0800

    add phpmyadmin

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php include_once("home.html"); ?>
+
+<!--
+mysql://b51848d70f78b8:740d6cc5@us-cdbr-east-03.cleardb.com/heroku_ecaa1a459ed3391?reconnect=true
+
+Username: b51848d70f78b8
+Password: 740d6cc5
+server:   us-cdbr-east-03.cleardb.com
+database: heroku_ecaa1a459ed3391
+
+-->
